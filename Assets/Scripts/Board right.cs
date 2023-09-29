608390efa9d87bf7ef4a8c4a23805571c9b234f2
NickSteijvers <144125086+NickSteijvers@users.noreply.github.com>
2023-09-29 12:14:29 +0200

    Build 1

---
+++ Assets/Scripts/Board right.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Boardright : MonoBehaviour
+{
+    public float xPosition = 8f;
+    public float yPosition = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yPosition = yPosition + 0.001f;
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+    }
+}
